95b0c83c9128150e3fff437d799da86c00354745
Eduardo Lanchares <eduardo@elanchares.com>
2016-02-29 00:54:29 +0100

    Display matches found

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var path = require('path');
 var app = require('electron').app;
 var BrowserWindow = require('electron').BrowserWindow;
-var ipc = require('electron').ipcMain;
 
 app.on('window-all-closed', function() {
   app.quit();
